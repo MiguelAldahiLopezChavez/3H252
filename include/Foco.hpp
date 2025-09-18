fa0dd049f82c3d84ef279163bc904626449eb3f5
MiguelAldahiLopezChavez <a24310385@live.ceti.mx>
2025-09-18 01:55:04 +0000

    Practica 1

---
+++ include/Foco.hpp
@@ -0,0 +1,13 @@
+#pragma once 
+#include <EstadoFoco.hpp>
+class Foco
+{
+private:
+    EstadoFoco estadoActual;
+public:
+    Foco(){estadoActual=false;}
+    ~Foco(){}
+    void Encender(){}
+    void Apagar(){}
+    EstadoFoco MostrarEstado(){}
+};
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include <Foco.hpp>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    Foco f;
+    cout<<"Hola mundo"<<endl;
+    return 0;
+}
